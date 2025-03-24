a81a8cdf5b44d23b2983e5398b07062cdc5746a2
Kazunari Kaizu <kwaizu@gmail.com>
2025-03-24 14:14:43 +0900

    Change org

--- clone_repositories.sh
+++ clone_repositories.sh
@@ -1,3 +1,3 @@
-git clone git@github.com:fuku-inc/labcode-web-app.git
-git clone git@github.com:fuku-inc/labcode-log-server.git
-git clone git@github.com:fuku-inc/labcode-sim.git
+git clone git@github.com:ecell/labcode-web-app.git
+git clone git@github.com:ecell/labcode-log-server.git
+git clone git@github.com:ecell/labcode-sim.git
